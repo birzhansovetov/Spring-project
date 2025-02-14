25c2ce4e406b0f2ce0812d2cd3047c17d6e39f9a
Sovetov Birzhan <bsovetov301@gmail.com>
2025-02-14 19:10:34 +0500

    create the Bean

---
+++ src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "org.example")
+public class ProjectConfig {
+    @Bean
+    @Primary
+    public Animal animal() {
+        Animal animal = new Animal();
+        animal.setName("Bear");
+        return animal;
+    }
+    @Bean
+    public Animal animal2() {
+        Animal animal = new Animal();
+        animal.setName("Monkey");
+        return animal;
+    }
+    @Bean
+    public Zoo zoo(Animal animal) {
+        Zoo zoo = new Zoo(animal);
+        zoo.showAnimal();
+        return zoo;
+    }
+}
